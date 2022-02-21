00bbd0a068db282624154965c92d874f0704acc7
GP <gregpetropoulos@yahoo.com>
2022-02-21 16:59:47 -0500

    Fade in and out with framer motion version 4.1.17 implemented.

--- src/Components/FeedbackList.js
+++ src/Components/FeedbackList.js
@@ -1,22 +1,45 @@
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
-export const FeedbackList = ({ feedback, handleDelete}) => {
+import { motion, AnimatePresence } from 'framer-motion';
+export const FeedbackList = ({ feedback, handleDelete }) => {
   console.log(feedback);
 
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback yet</p>;
   }
+
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div key={item.id}
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          exit={{opacity:0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // *Non animated return below
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem
+  //         key={item.id}
+  //         item={item}
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 };
 FeedbackItem.propTypes = {
   feedback: PropTypes.arrayOf(
