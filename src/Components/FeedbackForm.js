438420c75eab9dc3815c443913b2f82d73932da4
GP <gregpetropoulos@yahoo.com>
2022-02-21 07:14:23 -0500

    Added a RatingSelect component to show the radio buttons ul/li's and peice of state for when a radio button is selected. Passed a prop from the form to the RatingSelect component to capture the +e.currentTarget.value and update the rating peice of state in the form.

--- src/Components/FeedbackForm.js
+++ src/Components/FeedbackForm.js
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
-
   const handleTextChange = (e) => {
-
     // * REALTIME VALIDATION
-if(text === ''){
-    setBtnDisabled(true)
-    setMessage(null)
-} else if(text !== '' && text.trim().length<=10){
-setMessage('Text must be at least 10 characters')
-setBtnDisabled(true)
-}else{
-    setMessage(null)
-    setBtnDisabled(false)
-}
-// *-------REALTIME VALIDATION
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    // *-------REALTIME VALIDATION
 
     // console.log(e.target.value);
     setText(e.target.value);
@@ -30,7 +30,7 @@ setBtnDisabled(true)
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* ! TODO  - rating select component*/}
+        <RatingSelect select={(rating)=> setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
@@ -39,7 +39,9 @@ setBtnDisabled(true)
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
         {message && <div className='message'>{message}</div>}
       </form>
