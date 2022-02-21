7439d5a494744375e6f23d40ec931db4e738c742
GP <gregpetropoulos@yahoo.com>
2022-02-21 06:17:14 -0500

    Added a custom button component to be shared through out. Added default props to the custom button and brought it into the form. Props include type, version, isDisabled,and children.

--- src/Components/FeedbackForm.js
+++ src/Components/FeedbackForm.js
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 const FeedbackForm = () => {
-  
-  const [text,setText]=useState('')
+  const [text, setText] = useState('');
 
-const handleTextChange = e => {
+  const handleTextChange = (e) => {
     // console.log(e.target.value);
-    setText(e.target.value)
-}
-    return (
+    setText(e.target.value);
+  };
+  return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* ! TODO  - rating select component*/}
         <div className='input-group'>
-          <input type='text' className='' placeholder='Write a review' onChange={handleTextChange} 
-          value={text}/>
-          <button type="submit">Send</button>
+          <input
+            type='text'
+            className=''
+            placeholder='Write a review'
+            onChange={handleTextChange}
+            value={text}
+          />
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
