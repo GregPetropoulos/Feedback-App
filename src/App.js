9937b6df57e8c7d9419ba9e49ded7c62665dcb1a
GP <gregpetropoulos@yahoo.com>
2022-02-22 05:59:33 -0500

    Installed react-router-dom version 6 added an about page route and placed the rest of the app.js level components in a path='/' route.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackStat from './components/FeedbackStat';
 import FeedbackForm from './components/FeedbackForm';
 import { v4 as uuidv4 } from 'uuid';
+import About from './pages/About';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
@@ -15,22 +18,36 @@ function App() {
   };
 
   const addFeedback = (newFeedback) => {
-    //   Added the uuid function to give the submitted data a unique id
+    //*Added the uuid function to give the submitted data a unique id
     newFeedback.id = uuidv4();
 
-    // To add the feedback to the state set to an array and get a copy ...feedback and putting into the array, then put the new feedback at the front of the array
-    setFeedback([newFeedback,...feedback])
+    //*To add the feedback to the state set to an array and get a copy ...feedback and putting into the array, then put the new feedback at the front of the array
+    setFeedback([newFeedback, ...feedback]);
     console.log(newFeedback);
   };
   return (
-    <Fragment>
+    <Router>
       <Header />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStat feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        {/* Routes replaced the old <Switch> from version 5 react-router-dom */}
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStat feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }></Route>
+        <Route path='/about' element={<About />} />
+        </Routes>
       </div>
-    </Fragment>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+
+const About = () => {
+  return (<Card>
+<div className="about">
+
+  <h1>About this project</h1>
+  <p>This is a react app to leave feedback fro product or service</p>
+  <p>Version: 1.0.0</p>
+  <p>
+    
+  </p>
+</div>
+
+  </Card>
+  )
+}
+
+export default About
